0ab10f4a5f565115b1dbc2af053a9507991413fb
LucianSander <sanderluciansander2015@gmail.com>
2023-08-09 21:08:02 -0300

    Desvendando classes

--- php04/index.php
+++ php04/index.php
@@ -1,8 +1,17 @@
 <?php
     class Login{
+    //atributos da classe
         private $email;
         private $senha;
 
+    //passando pelo método de construção
+    public function __construct($email, $senha)
+    {
+        $this->setEmail($email);
+        $this->setSenha($senha);
+    }
+
+    //passando pelos métodos get e set
     public function getEmail(){
         return $this->email;
     }
@@ -19,6 +28,7 @@
         $this->senha = $s;
     }
 
+    //método da classa
     public function Logar(){
         if($this->email == "lucian@gmail.com" and $this->senha == "123456"):
             echo "Logado com Sucesso";
@@ -26,9 +36,10 @@
             echo "Dados Invalidos";
         endif;
     }
-}   
-$logar = new Login();
-$logar->setEmail("lucian@gmail.com");
-$logar->setSenha("123456");
+}
+
+//instanciando a classe
+$logar = new Login("lucian@gmail.com", "123456");
+//chamando o método
 $logar->Logar();
 ?>
